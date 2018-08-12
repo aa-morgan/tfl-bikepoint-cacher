17617c3875df49ffac58c250eddee3a464e189fe
Alex Morgan <axm108@gmail.com>
2018-08-12 23:01:54 +0100

    Refactored code and added try except around the bikepoints code to catch unexpected returns

--- bikepointcacher/BikePointCacher.py
+++ bikepointcacher/BikePointCacher.py
@@ -42,53 +42,75 @@ class BikePointCacher(object):
 
     def start(self):
         # Get parameters from config file
-        upload_loop_wait_time = int(self.config['PARAMS']['upload_loop_wait_time'])
-        download_loop_wait_time = int(self.config['PARAMS']['download_loop_wait_time'])
-        units = self.config['PARAMS']['units']
-        upload_loops = int(self.config['PARAMS']['upload_loops'])
-        tmp_data_dir = self.config['PARAMS']['tmp_data_dir']
-        zip_data_dir = self.config['PARAMS']['zip_data_dir']
-        verbose = bool(self.config['PARAMS']['verbose'])
+        self.num_upload_loops = int(self.config['PARAMS']['num_upload_loops'])
+        self.upload_loop_wait_time = int(self.config['PARAMS']['upload_loop_wait_time'])
+        self.download_loop_wait_time = int(self.config['PARAMS']['download_loop_wait_time'])
+        self.units = self.config['PARAMS']['units']
+        self.tmp_data_dir = self.config['PARAMS']['tmp_data_dir']
+        self.zip_data_dir = self.config['PARAMS']['zip_data_dir']
+        self.verbose = bool(self.config['PARAMS']['verbose'])
         
-        _units, label = get_units(units)
-        if upload_loops > 0: loop_desc = '{}'.format(upload_loops)
-        else: loop_desc = 'infinite'
+        units_mul, unit_label = get_units(self.units)
+        if self.num_upload_loops > 0: self.upload_loop_max = '{}'.format(self.upload_loops)
+        else: self.upload_loop_max = 'infinite'
+            
+        self.upload_loop_wait_time = self.upload_loop_wait_time * units_mul
+        self.download_loop_wait_time  = self.download_loop_wait_time  * units_mul
         
-        if verbose==True:
+        if self.verbose==True:
             print(
                 """Starting TfL BikePoint Cacher:
                 \t Download from TfL: every {} {}
                 \t Upload to Google Drive: every {} {}, {} time(s)
                 \t Local temporary data directory: {}
                 \t Local zipped data directory: {}
-                """.format(download_loop_wait_time, label, upload_loop_wait_time, label, loop_desc,
-                          tmp_data_dir, zip_data_dir))
+                """.format(self.download_loop_wait_time, unit_label, 
+                           self.upload_loop_wait_time, unit_label, 
+                           self.upload_loop_max, self.tmp_data_dir, self.zip_data_dir))
         
-        _upload_loop_wait_time = upload_loop_wait_time * _units
-        _download_loop_wait_time  = download_loop_wait_time  * _units
-        
-        payload = {'app_id': self.api_id, 'app_key': self.api_key}
+        self.payload = {'app_id': self.api_id, 'app_key': self.api_key}
 
-        ### Upload Loop Counter ###
-        _upload_loops = upload_loops
-        upload_loop_count = 0
-        while not _upload_loops == 0:
+        ### Upload Loops ###
+        self.upload_loops()
+            
+    def upload_loops(self):
+        """
+        """
+        self.upload_loop_remaining = self.num_upload_loops
+        self.upload_loop_count = 0
+        while not self.upload_loop_remaining == 0:
+            self.upload_loop_start_time = time.time() # in secs
+            self.upload_loop_remaining -= 1
+            self.upload_loop_count += 1
 
-            ### Upload Loop ###
-            _upload_loops -= 1
-            upload_loop_count += 1
-            # Upload to Google Drive after X minute(s)
-            _desc = 'Upload loop {}/{}'.format(upload_loop_count, loop_desc)
-            pbar = tqdm(total=(_upload_loop_wait_time//_download_loop_wait_time), desc=_desc, disable=not(verbose))
-            _upload_loop_start_time = time.time() # in secs
-            while (time.time() - _upload_loop_start_time) <= _upload_loop_wait_time:
-                # Start time
-                _download_loop_start_time = time.time() # in secs
+            ### Download Loops ###
+            self.download_loops()
 
-                # Request new bikepoint data
-                bikepoints = requests.get(bikepoint_endpoint, params=payload).json()
+            # Condense tmp csv files into gzip
+            filename = csv_to_gzip(self.config_filepath, verbose=self.verbose)
 
-                # Extract just data relating to bike availability
+            # Save to Google Drive
+            local_filepath = os.path.join(self.zip_data_dir, filename)
+            remote_filename = filename
+            upload_GDrive(self.config_filepath, local_filepath, remote_filename, 
+                          remote_folder_id=self.remote_folder_id, 
+                          mimetype='application/zip', verbose=self.verbose)
+
+    def download_loops(self):
+        """
+        """
+        # Upload to Google Drive after X minute(s)
+        tqdm_desc = 'Upload loop {}/{}'.format(self.upload_loop_count, self.upload_loop_max)
+        pbar = tqdm(total=(self.upload_loop_wait_time//self.download_loop_wait_time), 
+                    desc=tqdm_desc, disable=not(self.verbose))
+        while (time.time() - self.upload_loop_start_time) <= self.upload_loop_wait_time:
+            self.download_loop_start_time = time.time() # in secs
+
+            # Request new bikepoint data
+            bikepoints = requests.get(bikepoint_endpoint, params=self.payload).json()
+
+            # Extract just data relating to bike availability
+            try:
                 bikepoint_dict = {}
                 for bikepoint in bikepoints:
                     bikepoint_dict[bikepoint['id']] = {item['key']:item['value'] 
@@ -97,29 +119,24 @@ class BikePointCacher(object):
                 bikepoint_df = pd.DataFrame(bikepoint_dict)
 
                 # Save to file
-                if not os.path.isdir(tmp_data_dir):
-                    os.mkdir(tmp_data_dir)
+                if not os.path.isdir(self.tmp_data_dir):
+                    os.mkdir(self.tmp_data_dir)
                 bikepoint_df.to_csv(
-                    os.path.join(tmp_data_dir, 
+                    os.path.join(self.tmp_data_dir, 
                     '{}.csv'.format(time.strftime("%Y-%m-%d_%H:%M:%S")))
                 )
 
                 # Update progress bar
                 pbar.update(1)
 
-                ### Download Loop ###
-                while (time.time() - _download_loop_start_time) <= _download_loop_wait_time:
+                # Wait for next Download Loop iteration
+                while (time.time() - self.download_loop_start_time) <= self.download_loop_wait_time:
                     time.sleep(0.1) #  check every 0.1 secs
-
-            # Condense tmp csv files into gzip
-            filename = csv_to_gzip(self.config_filepath, verbose=verbose)
-            
-            # Save to Google Drive
-            local_filepath = os.path.join(zip_data_dir, filename)
-            remote_filename = filename
-            upload_GDrive(self.config_filepath, local_filepath, remote_filename, remote_folder_id=self.remote_folder_id, 
-                          mimetype='application/zip', verbose=verbose)
-
+                    
+            except:
+                # If failed then wait 10 sec and retry.
+                time.sleep(10)
+                
 def get_units(units):
     if units in ['d', 'day', 'days']:
         return (60*60*24, 'day(s)')
